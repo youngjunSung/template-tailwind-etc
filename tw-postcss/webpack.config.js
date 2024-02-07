e6fff9675be2b602f286d196137298312d4d77bd
성영 <youngjunsung@hecto.co.kr>
2024-02-07 16:15:59 +0900

    feat: init

---
+++ tw-postcss/webpack.config.js
@@ -0,0 +1,29 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  entry: './src/index.js',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'bundle.js',
+    publicPath: '/',
+    clean: true,
+  },
+  plugins: [new HtmlWebpackPlugin({ template: 'src/index.html' })],
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        include: path.resolve(__dirname, 'src'),
+        use: ['style-loader', 'css-loader', 'postcss-loader'],
+      },
+    ],
+  },
+  devServer: {
+    static: { directory: path.join(__dirname, 'dist') },
+    open: true,
+    port: 'auto',
+  },
+  devtool: 'source-map',
+  mode: 'development',
+};
